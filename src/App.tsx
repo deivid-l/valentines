384e6dadebdfb20ab89e52deaff93fe437090f6f
DRrojas <dsrojaslop@gmail.com>
2025-02-14 15:54:53 -0500

    Ajuste

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { WelcomePage } from './pages/welcome/WelcomePage';
 import { Home } from './pages/home/Home';
 import { Three } from './pages/happypage/Happy';
@@ -15,14 +15,14 @@ export const App: React.FC = () => {
   return (
     <div className='app-container'>
       <AnimatePresence>
-        <BrowserRouter basename={basename}>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<WelcomePage name={name} setName={setName} />} />
             <Route path="/home/:randomUrlPart" element={<Home name={name} setName={setName} />} />
             <Route path="/question" element={<Question name={name} setName={setName} />} />
             <Route path="/happy" element={<Three />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </AnimatePresence>
     </div>
   );
